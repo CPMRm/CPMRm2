6d3dcf4ecd3db726558a46402373b3c454c1972e
CPMRm <rmcpmstore@gmail.com>
2025-05-07 16:03:49 +0800

    Update cpmrm.py

--- cpmrm.py
+++ cpmrm.py
@@ -345,10 +345,5 @@ class CPMRm:
         response_decoded = response.json()
         return response_decoded.get("ok")
         
-   def unlock_crown(self) -> bool:
-        payload = { "account_auth": self.auth_token }
-        params = { "key": self.access_key }
-        response = requests.post(f"{__ENDPOINT_URL__}/unlock_crown", params=params, data=payload)
-        response_decoded = response.json()
-        return response_decoded.get("ok")
+ 
    
