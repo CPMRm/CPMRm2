da3a1f501aa2543a86ded1de5fdfbc9fd38f22eb
CPMRm <rmcpmstore@gmail.com>
2025-05-11 13:20:33 +0800

    Update cpmrm.py

--- cpmrm.py
+++ cpmrm.py
@@ -4,7 +4,7 @@ import requests
 import subprocess
 import urllib.parse
 
-__ENDPOINT_URL__: str = "https://changrydergentwo.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://changrydergen2.squareweb.app/api"
 
 class CPMRm:
     def __init__(self, access_key) -> None:
