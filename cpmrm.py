4bf4421e4593c0334ea2c5de89a7d4eccd704065
CPMRm <rmcpmstore@gmail.com>
2025-05-13 16:37:35 +0800

    Update cpmrm.py

--- cpmrm.py
+++ cpmrm.py
@@ -423,4 +423,7 @@ class CPMRm:
         payload = { "account_auth": self.auth_token, "car_id": car_id }
         params = {"key": self.access_key}
         response = requests.post(f"{__ENDPOINT_URL__}/car_info", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("ok")
+
         return response_decoded.get("ok")
