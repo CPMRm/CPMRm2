58d79002cb333424bee99777863b59af2ae90f6a
CPMRm <rmcpmstore@gmail.com>
2025-05-13 16:57:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
             load_player_data(cpm)
             load_key_data(cpm)
             load_client_details()
-            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28" ,"29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43"]
+            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28" ,"29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44"]
             print(Colorate.Horizontal(Colors.rainbow, '{01}: 增加綠鈔                 1.5K'))
             print(Colorate.Horizontal(Colors.rainbow, '{02}: 增加C幣                  4.5K'))
             print(Colorate.Horizontal(Colors.rainbow, '{03}: 皇冠等級                 8K'))
@@ -206,6 +206,7 @@ if __name__ == "__main__":
             print(Colorate.Horizontal(Colors.rainbow, '{41}: 移除男性角色頭部           3K'))
             print(Colorate.Horizontal(Colors.rainbow, '{42}: 移除女性角色頭部           3K'))
             print(Colorate.Horizontal(Colors.rainbow, '{43}: 車輛資訊(用車輛id查看      免費'))
+            print(Colorate.Horizontal(Colors.rainbow, '{44}: 完成全部停車關卡           1K'))
             print(Colorate.Horizontal(Colors.rainbow, '{0} : 退出腳本'))
             
             print(Colorate.Horizontal(Colors.rainbow, '===============[ 𝐂𝐏𝐌 ]==============='))
