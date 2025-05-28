1bec975d95951eb5cd50317f5526e14a69170c8f
CPMRm <rmcpmstore@gmail.com>
2025-05-28 15:00:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -68,6 +68,9 @@ def load_player_data(cpm):
             print(Colorate.Horizontal(Colors.rainbow, f'綠鈔   : {data.get("money")}.'))
             
             print(Colorate.Horizontal(Colors.rainbow, f'C幣    : {data.get("coin")}.'))
+
+            print(Colorate.Horizontal(Colors.rainbow, f'車輛    : {data.get("car")}.'))
+            print(Colorate.Horizontal(Colors.rainbow, f'好友    : {data.get("friends")}.'))
             
         else:
             print(Colorate.Horizontal(Colors.rainbow, '! ERROR: new accounts most be signed-in to the game at least once !.'))
